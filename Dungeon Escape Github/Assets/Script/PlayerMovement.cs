cd436879d4abcfa25a5a8ce94d437e27c556c99b
Asbaq <asbaqlaareb45@gmail.com>
2022-07-07 00:56:22 +0530

    Update

--- Dungeon Escape Github/Assets/Script/PlayerMovement.cs
+++ Dungeon Escape Github/Assets/Script/PlayerMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformlayerMask;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator anim;
+    private BoxCollider2D boxCollider2d;
 
     private float dirX = 0f;
     [SerializeField] private float movespeed = 5f;
-    [SerializeField] private float jumpforce = 7f;
+    [SerializeField] private float jumpforce = 3f;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,15 +20,18 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //Horizontal run
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * movespeed,rb.velocity.y);
 
-         if(Input.GetKey("space"))
+        //Jump
+         if(IsGrounded() && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpforce);
         } 
@@ -34,6 +39,12 @@ public class PlayerMovement : MonoBehaviour
         UpdateAnimationUpdate();
     }
 
+    private bool IsGrounded() {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f,platformlayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
+    }
+    //Animation
     private void UpdateAnimationUpdate()
     {
           if(dirX > 0f)
