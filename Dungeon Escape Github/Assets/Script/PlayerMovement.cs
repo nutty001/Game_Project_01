ed1fc31a83c8eb13e39fb44ebf348f44e4f00aea
Asbaq <asbaqlaareb45@gmail.com>
2022-07-06 23:29:45 +0530

    Update 3

--- Dungeon Escape Github/Assets/Script/PlayerMovement.cs
+++ Dungeon Escape Github/Assets/Script/PlayerMovement.cs
@@ -4,35 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField] private float movespeed = 5f;
+    [SerializeField] private float jumpforce = 7f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 5f,rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * movespeed,rb.velocity.y);
 
          if(Input.GetKey("space"))
         {
-            rb.velocity = new Vector2(rb.velocity.x,7f);
+            rb.velocity = new Vector2(rb.velocity.x,jumpforce);
         } 
 
-        if(dirX > 0f)
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+          if(dirX > 0f)
         {
             anim.SetBool("Running",true);
+            sprite.flipX = false;
         }
         else if(dirX < 0f)
         {
             anim.SetBool("Running",true);
+            sprite.flipX = true;
         }
         else
         {
